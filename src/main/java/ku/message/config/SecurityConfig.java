30e466cf6ec18989b4021f7a7b845acb51cc98c8
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-18 09:58:09 +0700

    Delete the AuthenticationService class
    Since we’ll use a default authentication service that comes with Spring Security instead of creating our own. But we need to supply a UserDetailsService class as shown below.

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ku.message.config;
 
-import ku.message.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
